ffb245af5086c8e9b369ee9c7ec7a793d5664703
Jaychopra932 <jaychopra932@gmail.com>
2025-12-30 12:51:45 +0530

    Update API URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "https://bookmyclean.onrender.com", // backend URL
   headers: {
     "Content-Type": "application/json",
   },
